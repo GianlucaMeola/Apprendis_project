9bedabfc08f2e0c6ca35c850332eea9e861b0404
Gianluca Meola <Gianluca.Meola@tlcmarketing.com>
2019-05-01 17:55:56 +0100

    01/05/2019 new day create just front end with some validations

--- Angular7/src/app/shared/user.service.ts
+++ Angular7/src/app/shared/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+
+  formModel = this.fb.group({
+    UserName:['', Validators.required],
+    Email:['', Validators.email],
+    FullName:[''],
+    Passwords: this.fb.group({
+      Password:['', [Validators.required, Validators.minLength(4)]],
+      ConfirmPassword:['', Validators.required]
+    },{validator : this.comparePasswords})
+  });
+
+  comparePasswords(fb: FormGroup){
+    let ConfirmPasswordCtrl = fb.get('ConfirmPassword');
+    //passwordMissmatch
+    //confirmPasswordCtrl.errors={passwordMissmatch: true}
+    if(ConfirmPasswordCtrl.errors == null || 'passwordMismatch' in ConfirmPasswordCtrl.errors){
+      if(fb.get('Password').value != ConfirmPasswordCtrl.value )
+        ConfirmPasswordCtrl.setErrors({passwordMismatch: true});
+      else
+        ConfirmPasswordCtrl.setErrors(null)
+    }
+  }
 }
